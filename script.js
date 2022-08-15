45b4db5abd61f5cfa089bf529c955a7caea864c0
Jared Peter <jaredpeter06@icloud.com>
2022-08-15 17:01:23 -0300

    Added draw functionality and remove borders function

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ const makeGrid = num => {
     grid.innerHTML = '';
 
     for (let i = 0; i < num * num; i++) {
+        if (num > 100) num = 100;
 
         const item = document.createElement('div');
+
         item.classList.add('item');
+
+        item.addEventListener('mouseenter', () => {
+            item.style.background = 'black';
+        });
 
         grid.appendChild(item);
         grid.style.gridTemplateColumns = `repeat(${num}, 2fr)`;
         grid.style.gridTemplateRows = `repeat(${num}, 2fr)`;
     }
+}
+
+const removeBorders = () => {
+    const items = document.getElementsByClassName('item');
+
+    for (const item of items) {
+        item.setAttribute('style', 'border: none')
+    }
+
+    // console.log(items);
 }
 
 const changeGrid = () => {
